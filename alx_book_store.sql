82e19a43a41377cad3edd9219ffd047564c39294
unknown <yusuff4u2c@gmail.com>
2025-06-29 16:40:32 +0100

    db

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -5,13 +5,13 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 -- Create the AUTHORS table
-CREATE TABLE AUTHORS (
+CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 -- Create the BOOKS table
-CREATE TABLE BOOKS (
+CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
@@ -21,7 +21,7 @@ CREATE TABLE BOOKS (
 );
 
 -- Create the CUSTOMERS table
-CREATE TABLE CUSTOMERS (
+CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE CUSTOMERS (
 );
 
 -- Create the ORDERS table
-CREATE TABLE ORDERS (
+CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
@@ -37,7 +37,7 @@ CREATE TABLE ORDERS (
 );
 
 -- Create the ORDER_DETAILS table
-CREATE TABLE ORDER_DETAILS (
+CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
